9bbfb51fa6603ce195ebd8e6ab516f6af5907dde
Wisterleys <wister.sousa2@gmail.com>
2021-07-01 19:52:37 -0300

    Controles para iniciar o game e parar

--- assets/js/Controllers/Controller.js
+++ assets/js/Controllers/Controller.js
@@ -1,18 +1,28 @@
 class Controller{
     constructor(data){
         this._game_area = data.gameArea;
+        this._main_loop;
         //Methods
-        this.start()
+       this.onStart()
+    }
+    onStart(){
+        $("#start").on("click",e=>{
+            this.start()
+        })
+        $("#stop").on("click",e=>{
+            clearInterval(this.main_loop)
+        })
     }
     start(){
-        setInterval(e=>{
+        this.main_loop=setInterval(e=>{
             new Barriers([this.game_area,this.getPosition(this.game_area)],{color:["#639301","#a5e82e"],vel:10});
         },4000)
     }
     getPosition(el){
         return el.getBoundingClientRect()
     }
-    
+    get main_loop(){return this._main_loop}
+    set main_loop(value){this._main_loop=value}
     get game_area(){return this._game_area}
     set game_area(value){this._game_area=value}
 }
