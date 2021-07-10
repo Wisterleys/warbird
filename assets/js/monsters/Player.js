397f89392ee7a77f94663b91dd3c795166ef7d14
Wisterleys <wister.sousa2@gmail.com>
2021-07-10 13:39:03 -0300

    Criado classe Player sem teste

---
+++ assets/js/monsters/Player.js
@@ -0,0 +1,80 @@
+class Player{
+    constructor(place,dis,name){
+        this._place = place;
+        this._direction=0;
+        this._vel=70;
+        this._mainLoop;
+        this.move(this.template(this.place,dis,name))
+        this.onControl()
+    }
+    die(I){
+        const IHave = this.getPosition(I)
+        for (let i = 0; i < $(".b").length; i++) {
+            const barr_a = this.getPosition($(".a")[i]);
+            const barr_b = this.getPosition($(".b")[i]);
+            if(IHave.x+IHave.width>=barr_b.x&&IHave.x+IHave.width<=barr_b.x+barr_b.width&&IHave.y+IHave.height>=barr_b.y
+                ||
+                IHave.x+IHave.width>=barr_a.x&&IHave.x+IHave.width<=barr_a.x+barr_a.width&&IHave.y<=barr_a.y+barr_a.height){
+                clearInterval(this.mainLoop)
+                I.$("img")[0].src="assets/images/explosion.gif"
+                setTimeout(()=>{I.remove();},800)
+            }
+        }
+    }
+    choice(value){
+        switch(value){
+            case"up":
+            this.direction=-1
+            break;
+            case"down":
+            this.direction=1
+            break;
+        }
+    }
+    onControl(){
+        $(".btn-control").forEach(btn => {
+            btn.on("touchstart mousedown",e=>{
+                this.choice(e.target.id)
+            })
+            btn.on("touchend mouseup",e=>{
+                this.direction=0
+            })
+        });
+    }
+    move(el){
+        this.mainLoop=setInterval(()=>{
+            $(".barreira")[0]?this.direction=this.intelligence(el):0
+            $(".barreira")[0]?this.die(el):0
+            if($("#control-all").value=="true"){
+                el.style.top=el.offsetTop+(this.direction*this.vel)+"px";
+            }
+        },100)
+    }
+    template(el,dis,name=""){
+        /*
+            <div id="playerPC">
+            <img src="assets/images/passaro.png" alt="passaro">
+            </div>
+        */
+       let e = el.addEl({tag:"div",id:"playerPC"})
+       e.addEl({tag:"div",insertTag:name,style:"margin:0px;padding:0px"})
+       e.addEl({tag:"img",src:"assets/images/passaro.png",alt:"passaro"})
+       const place = this.getPosition(this.place)
+       e.style.top=place.height/2+"px"
+       e.style.left=place.width/dis+"px"
+       return e;
+    }
+    getPosition(el){
+        return el.getBoundingClientRect()
+    }
+    //GETs and SETs
+    
+    get mainLoop(){return this._mainLoop;}
+    set mainLoop(value){this._mainLoop=value}
+    get direction(){return this._direction;}
+    set direction(value){this._direction=value}
+    get vel(){return this._vel;}
+    set vel(value){this._vel=value}
+    get place(){return this._place}
+    set place(value){this._place=value}
+}
